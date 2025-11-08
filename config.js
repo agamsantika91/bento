09f52b0b266d08f34efb067d485b6faa608ba13b
Agam Santika <71170896+agamsantika91@users.noreply.github.com>
2025-11-08 17:55:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -87,7 +87,7 @@ const CONFIG = {
 		{
 			id: '5',
 			name: 'Whatsapp Web',
-			icon: 'https://fontawesome.com/icons/whatsapp?f=brands&s=solid',
+			icon: 'f232',
 			link: 'https://web.whatsapp.com/',
 		},
 		{
